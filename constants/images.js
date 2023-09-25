b0dc13bc1f3d2a15ac2fcf5b4009fc67d00800ec
NoobCoder_69 <vishant448@gmail.com>
2023-09-25 15:06:58 +0530

    React-native app

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "D:/Vscodes/react_native/assets/images/profile_icon.png";
+
+export default {
+  profile,
+};
---
+++ hook/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "7b50a9f35dmshbdcaf885c01e76fp1c16d7jsn3e6c33fedeb4",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+
+    params: { ...query },
+  };
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isloading, error, refetch };
+};
+export default useFetch;
